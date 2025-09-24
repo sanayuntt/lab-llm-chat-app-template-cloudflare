ceb7963efa1f6d283fcb9aa2a641be4f4265d7e3
sanayuntt <sanayu.rungraungdee@global.ntt>
2025-09-24 14:25:52 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ async function handleChatRequest(
         returnRawResponse: true,
         // Uncomment to use AI Gateway
          gateway: {
-           id: aigateway, // Replace with your AI Gateway ID
+           id: "aigateway", // Replace with your AI Gateway ID
            skipCache: false,      // Set to true to bypass cache
            cacheTtl: 3600,        // Cache time-to-live in seconds
          },
